85a5b99fcc552072b39fbe88c1d43390ce9173d0
MerryLeo <leotaillon2@gmail.com>
2022-04-22 12:58:03 -0400

    Added Sort Class with BubbleSort method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intArr = new List<int> { 1, -8, 3, 5, 129, -39, 42, -5, 2, -10 };
+            intArr.BubbleSort();
+            Console.WriteLine(string.Join(", ", intArr));
+            
+            List<string> stringArr = new List<string> {"Leo", "Benjamin", "Julie", "Patrick"};
+            stringArr.BubbleSort();
+            Console.WriteLine(string.Join(", ", stringArr));
+        }
+    }
+}
