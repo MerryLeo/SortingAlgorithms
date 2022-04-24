314f02c560b475fd85d8da32bd9787c080f5ae06
MerryLeo <leotaillon2@gmail.com>
2022-04-24 17:52:16 -0400

    Adding User Input without Validation

--- Program.cs
+++ Program.cs
@@ -4,21 +4,77 @@
     {
         static void Main(string[] args)
         {
-            List<int> myIntegers = new List<int> {5, -10, -252, 105, 8, 5, -102, 55, -202, -32, 102};
-            List<int> copyOfList = new List<int>(myIntegers);
-            SortStatistic stat = copyOfList.BubbleSort();
-            Console.WriteLine($"Bubble Sort returned: {string.Join(", ", myIntegers)}");
-            stat.PrintStatistic();
+            // Get type of data to sort from user
+            string? input;
+            Console.Write("What kind of data do you want to sort? (T)ext or (N)umbers: ");
+            input = Console.ReadLine();
+            if (input != "T" && input != "N")
+            {
+                bool validInput = false;
+                while (!validInput)
+                {
+                    Console.WriteLine("Your input was invalid. Try again (T)ext or (N)umbers: ");
+                    input = Console.ReadLine();
+                    validInput = input == "T" || input == "N";
+                }
+            }
+            
+            if (input == "T")
+            {
+                Console.Write("\nEnter a list of words seperated by a comma and a space (ex: Apple, Orange, Green): ");
+                input = Console.ReadLine() ?? throw new ArgumentNullException();
+                List<string> list = input.Split(", ").ToList();
+                
+                // Print Results
+                Console.WriteLine("\nResults: ");
 
-            copyOfList = new List<int>(myIntegers);
-            stat = copyOfList.CocktailSort();
-            Console.WriteLine($"\nCocktail Sort returned: {string.Join(", ", myIntegers)}");
-            stat.PrintStatistic();
+                // Sort with Bubble Sort
+                List<string> copyList = new List<string>(list);
+                SortStatistic statistic = copyList.BubbleSort();
+                Console.WriteLine($"Bubble Sort returned: {string.Join(", ", copyList)}");
+                statistic.PrintStatistic();
 
-            copyOfList = new List<int>(myIntegers);
-            stat = copyOfList.QuickSort();
-            Console.WriteLine($"\nQuick Sort returned: {string.Join(", ", myIntegers)}");
-            stat.PrintStatistic();
+                // Reset List and Sort with Quick Sort
+                copyList = new List<string>(list);
+                statistic = copyList.QuickSort();
+                Console.WriteLine($"\nQuick Sort returned: {string.Join(", ", copyList)}");
+                statistic = copyList.QuickSort();
+                statistic.PrintStatistic();
+
+                // Reset List and Sort with Cocktail Sort
+                copyList = new List<string>(list);
+                statistic = copyList.CocktailSort();
+                Console.WriteLine($"\nCocktail Sort returned: {string.Join(", ", copyList)}");
+                statistic.PrintStatistic();
+            }
+            else
+            {
+                Console.Write("\nEnter a list of numbers seperated by a comma and a space (ex: 1, -23, 5, 102): ");
+                input = Console.ReadLine() ?? throw new ArgumentNullException();
+                List<float> list = input.Split(", ").Select(x => float.Parse(x)).ToList();
+
+                // Print Results
+                Console.WriteLine("\nResults: ");
+
+                // Sort with Bubble Sort
+                List<float> copyList = new List<float>(list);
+                SortStatistic statistic = copyList.BubbleSort();
+                Console.WriteLine($"Bubble Sort returned: {string.Join(", ", copyList)}");
+                statistic.PrintStatistic();
+
+                // Reset List and Sort with Quick Sort
+                copyList = new List<float>(list);
+                statistic = copyList.QuickSort();
+                Console.WriteLine($"\nQuick Sort returned: {string.Join(", ", copyList)}");
+                statistic = copyList.QuickSort();
+                statistic.PrintStatistic();
+
+                // Reset List and Sort with Cocktail Sort
+                copyList = new List<float>(list);
+                statistic = copyList.CocktailSort();
+                Console.WriteLine($"\nCocktail Sort returned: {string.Join(", ", copyList)}");
+                statistic.PrintStatistic();
+            }
         }
     }
 }
